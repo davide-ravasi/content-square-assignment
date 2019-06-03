a11f72f7f271ea75f09eedfab98f919e354760b7
davide-ravasi <you@example.com>
2019-06-03 13:13:26 +0200

    repos service added return type

--- src/app/services/repos.service.ts
+++ src/app/services/repos.service.ts
@@ -1,6 +1,6 @@
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
 
 
@@ -10,13 +10,13 @@ export class GithubReposService {
 
   constructor(private http: Http) {}
 
-  searchRepos(queryTitle: string) {
+  searchRepos(queryTitle: string): Observable<any> {
     return this.http.get(`${this.API_PATH}/${queryTitle}/repos`)
       .map((response: Response) => {
         return response.json() || []
       });
   }
-  getUsersPhoto(queryName: string) {
+  getUsersPhoto(queryName: string): Observable<any> {
     return this.http.get(`${this.API_PATH}/${queryName}`)
       .map((res) => {
         return res.json();
